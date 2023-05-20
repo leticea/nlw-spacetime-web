ff5a529d19a49e98367089ddd2495237b65d7be0
leticea <nascimento.leticeah@gmail.com>
2023-05-20 19:06:23 -0300

    feat(page.tsx): right side styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <main></main>
+  return (
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div></div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="" className="underline hover:text-gray-50">criar agora</a>!
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
