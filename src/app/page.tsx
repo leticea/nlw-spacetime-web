3453d886d2caf3527e71f81459fbd546744fdd61
leticea <nascimento.leticeah@gmail.com>
2023-05-20 19:23:12 -0300

    feat(page.tsx): left side styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,19 @@ export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
-      <div></div>
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+      </div>
 
       {/* Right */}
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">
-          <p className="text-center leading-relaxed w-[360px]">
+          <p className="w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{" "}
-            <a href="" className="underline hover:text-gray-50">criar agora</a>!
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
           </p>
         </div>
       </div>
