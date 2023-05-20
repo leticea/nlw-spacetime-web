c718f8f3a39a3d65f1c7d67bbb0a47413c7d1063
leticea <nascimento.leticeah@gmail.com>
2023-05-20 18:55:47 -0300

    feat(layout.tsx): inserting colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-      <h1 className="text-4xl font-bold font-alt">Sua cápsula do tempo</h1>
-    </div>
-  );
+  return <main></main>
 }
