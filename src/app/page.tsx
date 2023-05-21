580c22adb55f1a220be4c7a48cb75c7ee724ae8f
leticea <nascimento.leticeah@gmail.com>
2023-05-21 09:58:13 -0300

    feat(page.tsx): left side styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,17 @@ export default function Home() {
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        {/* Bllur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
+        {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        <a href="" className="flex items-center gap-3 text-left">
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className="h-5 w-5 text-gray-500" />
           </div>
@@ -19,6 +25,9 @@ export default function Home() {
             memórias!
           </p>
         </a>
+
+        {/* Hero */}
+        
       </div>
 
       {/* Right */}
