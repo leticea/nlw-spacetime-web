f2b6f24ab31519a33277ff82ff02b8c08249cd76
leticea <nascimento.leticeah@gmail.com>
2023-05-21 10:18:21 -0300

    feat(page.tsx): hero styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,11 +35,11 @@ export default function Home() {
           <Image src={nlwLogo} alt="NLW Spacetime" />
 
           <div className="max-w-[420px] space-y-1">
-            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">Sua cápsula do tempo</h1>
-            <p>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">Sua cápsula do tempo</h1>
+            <p className="text-lg leading-relaxed">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
           </div>
 
-          <a href="" className="inline-block">CADASTRAR LEMBRANÇA</a>
+          <a href="" className="inline-block rounded-full bg-green-500 px-5 py-3 font-bold text-sm uppercase leading-none text-black hover:bg-green-600">CADASTRAR LEMBRANÇA</a>
         </div>
       </div>
 
