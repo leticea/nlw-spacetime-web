a9fae6cc060081b5e046eda4f2c9667b69e5a2ac
leticea <nascimento.leticeah@gmail.com>
2023-05-23 18:06:17 -0300

    feat(logout): creating logout route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
+import { Hero } from "@/components/Hero";
+import { Profile } from "@/components/Profile";
+import { SignIn } from "@/components/SignIn";
 import "./globals.css";
 import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from "next/font/google";
+import { Copyright } from "@/components/Copyright";
+import { cookies } from "next/headers";
 
 const robotoFlex = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
 
@@ -23,12 +28,34 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isAuthenticated = cookies().has("token");
+
   return (
     <html lang="en">
       <body
         className={`${robotoFlex.variable} ${jamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Bllur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+
+            <Hero />
+
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
