c441382ac842e6e48094205dc9d9bca0a7cae739
leticea <nascimento.leticeah@gmail.com>
2023-05-25 10:38:32 -0300

    feat(MediaPicker.tsx): creating onFileSelected function

--- src/components/MediaPicker.tsx
+++ src/components/MediaPicker.tsx
@@ -3,18 +3,36 @@
 import { ChangeEvent, useState } from "react";
 
 export function MediaPicker() {
-  const [] = useState()
+  const [preview, setPreview] = useState<string | null>(null);
 
   function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
-    console.log(event.target.files);
+    const { files } = event.target;
+
+    if (!files) {
+      return;
+    }
+
+    const previewURL = URL.createObjectURL(files[0]);
+
+    setPreview(previewURL);
   }
 
   return (
-    <input
-      onChange={onFileSelected}
-      type="file"
-      id="media"
-      className="invisible h-0 w-0"
-    />
+    <div>
+      <input
+        onChange={onFileSelected}
+        type="file"
+        id="media"
+        className="invisible h-0 w-0"
+      />
+
+      {preview && (
+        <img
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+      )}
+    </div>
   );
 }
