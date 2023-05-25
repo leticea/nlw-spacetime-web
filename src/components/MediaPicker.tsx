af88276923a12dcb3e547b14ef91671a9d566b60
leticea <nascimento.leticeah@gmail.com>
2023-05-25 10:27:51 -0300

    feat(MediaPicker.tsx): creating use client to render onChange

--- src/components/MediaPicker.tsx
+++ src/components/MediaPicker.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { ChangeEvent, useState } from "react";
+
 export function MediaPicker() {
+  const [] = useState()
+
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.files);
+  }
+
   return (
     <input
-      onChange={(value) => console.log(value)}
+      onChange={onFileSelected}
       type="file"
       id="media"
       className="invisible h-0 w-0"
