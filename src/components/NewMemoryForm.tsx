6b327df9bad64922b87133cdd8b203b07038b76c
leticea <nascimento.leticeah@gmail.com>
2023-05-25 11:40:42 -0300

    feat(NewMemoryForm.tsx): form submit

--- src/components/NewMemoryForm.tsx
+++ src/components/NewMemoryForm.tsx
@@ -15,7 +15,7 @@ export function NewMemoryForm() {
 
   return (
     <form
-      onSubmit={() => console.log("submit")}
+      onSubmit={handleCreateMemory}
       className="flex flex-1 flex-col gap-2"
     >
       <div className="flex items-center gap-4">
