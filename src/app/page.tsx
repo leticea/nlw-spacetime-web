202694f9db975955479481456907a101436a3b94
leticea <nascimento.leticeah@gmail.com>
2023-05-26 14:34:38 -0300

    feat(page.tsx): save memories in  the DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 import { cookies } from "next/headers";
-
 import { EmptyMemories } from "@/components/EmptyMemories";
+import { api } from "@/lib/api";
 
-export default function Home() {
-  return <EmptyMemories />;
+export default async function Home() {
+  const isAuthenticated = cookies().has("token");
+
+  if (!isAuthenticated) {
+    return <EmptyMemories />;
+  }
+
+  const token = cookies().get("token")?.value;
+  const response = await api.get("/memories", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const memories = response.data;
+
+  return <div>{JSON.stringify(memories)}</div>;
 }
