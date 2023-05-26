977259e69b6069a5c81cac2dc7e53ddb5030f516
leticea <nascimento.leticeah@gmail.com>
2023-05-26 15:29:02 -0300

    feat(page.tsx): memories styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
           </div>
 
           {/* Right */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
